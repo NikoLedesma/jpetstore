41801bd235e86ba070269f2b532fae986df86684
nikoled <nicolase.ledesma@gmail.com>
2020-06-21 15:29:26 -0300

    integration jacoco

---
+++ src/test/java/sample/formatter/CartItemTest.java
@@ -0,0 +1,47 @@
+package sample.formatter;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import sample.entity.Amount;
+import sample.entity.Item;
+import sample.model.CartItem;
+
+import java.math.BigDecimal;
+import java.util.Locale;
+
+public class CartItemTest {
+
+  private CartItem cartItem;
+
+  @Before
+  public void setUpFormater() {
+    cartItem = new CartItem();
+    Item item = new Item();
+    item.setListPrice(new Amount(new BigDecimal("25.50")));
+    cartItem.setItem(item);
+    cartItem.setInStock(true);
+    cartItem.setQuantity(10);
+  }
+
+
+  @Test
+  public void testTotal() {
+    Amount totalAmount=cartItem.getTotal();
+    Assert.assertEquals(new BigDecimal("255.00"), totalAmount.getValue());
+  }
+
+  @Test
+  public void testIncrement5Times() {
+    cartItem.incrementQuantity();
+    cartItem.incrementQuantity();
+    cartItem.incrementQuantity();
+    cartItem.incrementQuantity();
+    cartItem.incrementQuantity();
+    Amount totalAmount=cartItem.getTotal();
+    Assert.assertEquals(new BigDecimal("382.50"), totalAmount.getValue());
+  }
+
+
+
+}
---
+++ src/test/java/sample/formatter/CartTest.java
@@ -0,0 +1,77 @@
+package sample.formatter;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import sample.entity.Amount;
+import sample.entity.Item;
+import sample.model.Cart;
+
+import java.math.BigDecimal;
+
+public class CartTest {
+  Cart cart ;
+  String ID_ITEM_A="itemA";
+  String ID_ITEM_B="itemB";
+  String ID_ITEM_C="itemC";
+
+  @Before
+  public void setUpFormater() {
+    cart = new Cart();
+
+    Item itemA= new Item();
+    itemA.setItemId(ID_ITEM_A);
+    itemA.setListPrice(new Amount(new BigDecimal("5.00")));
+
+    Item itemB= new Item();
+    itemB.setItemId(ID_ITEM_B);
+    itemB.setListPrice(new Amount(new BigDecimal("10.00")));
+
+    cart.addItem(itemA,true);
+    cart.addItem(itemB,true);
+  }
+
+
+  @Test
+  public void testSubTotal() {
+    Amount  subTotal = cart.getSubTotal();
+    Assert.assertEquals(new BigDecimal("15.00"), subTotal.getValue());
+  }
+
+
+
+  @Test
+  public void testIncrementQuantityByItemId() {
+    cart.incrementQuantityByItemId(ID_ITEM_A);
+    cart.incrementQuantityByItemId(ID_ITEM_A);
+    cart.incrementQuantityByItemId(ID_ITEM_A);
+    cart.incrementQuantityByItemId(ID_ITEM_B);
+    cart.incrementQuantityByItemId(ID_ITEM_B);
+    Assert.assertEquals(2, cart.getNumberOfItems());
+    Assert.assertEquals(4, cart.getCartItem(ID_ITEM_A).getQuantity());
+    Assert.assertEquals(3, cart.getCartItem(ID_ITEM_B).getQuantity());
+  }
+
+   @Test
+  public void testClear() {
+    cart.clear();
+    Assert.assertEquals(0, cart.getNumberOfItems());
+    Assert.assertTrue(cart.isEmpty());
+  }
+
+  @Test
+  public void testAddItemWithQty() {
+    cart.isEmpty();
+    Item itemC= new Item();
+    itemC.setItemId(ID_ITEM_C);
+    itemC.setListPrice(new Amount(new BigDecimal("13.00")));
+    cart.addItem(itemC,true);
+    cart.setQuantityByItemId(ID_ITEM_C,10);
+    Assert.assertEquals(new BigDecimal("145.00"), cart.getSubTotal().getValue());
+    Assert.assertEquals(3, cart.getNumberOfItems());
+    Assert.assertEquals(1, cart.getCartItem(ID_ITEM_A).getQuantity());
+    Assert.assertEquals(1, cart.getCartItem(ID_ITEM_B).getQuantity());
+    Assert.assertEquals(10, cart.getCartItem(ID_ITEM_C).getQuantity());
+  }
+
+}
